c034c23c456abb25dea041b8809f7d6119905529
王璞 <501501369@qq.com>
2020-07-08 08:47:02 +0800

    升级组件库

---
+++ src/component/tooltip/tooltip.vue
@@ -0,0 +1,39 @@
+<template>
+  <pg-popper
+      class="pg-tooltip"
+      v-model="expend"
+      width="100px"
+      :placement="placement"
+      :append-arrow="true"
+      :trigger="trigger"
+      appendToBody
+  >
+    <span class="cursor-pointer">
+      <slot name="toggle">
+        <i class="icon-question4 text-secondary"></i>
+      </slot>
+    </span>
+    <div class="dropdown-box text-center p-10" slot="content">
+      <slot></slot>
+    </div>
+  </pg-popper>
+</template>
+
+<script>
+  import pgPopper from './../popper/popper';
+  export default {
+    name: 'pg-tooltip',
+    components: { pgPopper },
+    props: {
+      placement: { type: String, default: 'top' },
+      trigger: { type: String, default: 'hover', validator: v => ['click', 'hover'].includes(v) },
+    },
+    data() {
+      return {
+        expend: false,
+      }
+    },
+    methods: {
+    }
+  }
+</script>
